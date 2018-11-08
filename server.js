ff2b26bbad8110c3dc49ff318bab919bc2623396
Spencer <backmanspencer99@gmail.com>
2018-11-08 12:39:09 -0700

    configuring server

--- server.js
+++ server.js
@@ -15,10 +15,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
 app.use(morgan('dev'));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://mymusichub.herokuapp.com');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
+}
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(allowCrossDomain);
+    app.use(app.router);
+    app.use(express.static(__dirname, '/client'))
 })
 
 app.use('/tracks', expressJwt({secret: process.env.SECRET}));

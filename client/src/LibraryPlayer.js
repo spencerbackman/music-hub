36a0ad04278e369c6f441ab090d93f8efb2961bc
Spencer <backmanspencer99@gmail.com>
2018-11-21 16:18:40 -0700

    added origin

--- client/src/LibraryPlayer.js
+++ client/src/LibraryPlayer.js
@@ -21,6 +21,7 @@ class LibraryPlayer extends React.Component {
             proxy: false,
             maxRedirects: 1,
             Accept: 'application/json',
+            Origin: 'https://mymusichub.herokuapp.com',
             headers: {
                 'Access-Control-Allow-Origin': 'https://mymusichub.herokuapp.com',
                 "Access-Control-Allow-Headers": "X-Custom-Header, Upgrade-Insecure-Requests"
@@ -56,6 +57,7 @@ class LibraryPlayer extends React.Component {
             proxy: false,
             maxRedirects: 1,
             Accept: 'application/json',
+            Origin: 'https://mymusichub.herokuapp.com',
             headers: {
                 'Access-Control-Allow-Origin': 'https://mymusichub.herokuapp.com',
                 "Access-Control-Allow-Headers": "X-Custom-Header, Upgrade-Insecure-Requests"

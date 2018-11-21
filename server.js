be633636f0f94206dbb65f87a3d06dea69ddfd22
Spencer <backmanspencer99@gmail.com>
2018-11-21 16:27:36 -0700

    not sure

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 app.use(morgan('dev'));
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://mymusichub.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', 'https://mymusichub.herokuapp.com', 'localhost:4000');
     res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
     res.header('Access-Control-Allow-Credentials', true);
     next();
